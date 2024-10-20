df6447b599b434d283e6a58e810a59549f149a88
ANDouNo <younofl312@gmail.com>
2024-10-20 11:51:51 +0300

    small fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://andyouno.github.io/Soul/", // Убедитесь, что здесь правильный путь
+});
