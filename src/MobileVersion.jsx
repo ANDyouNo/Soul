818cf18fb8a92db0e01bef5491a8fd292fdec8b1
ANDouNo <younofl312@gmail.com>
2024-10-23 05:38:36 +0300

    light mobile styles

--- src/MobileVersion.jsx
+++ src/MobileVersion.jsx
@@ -1,10 +1,11 @@
 // src/MobileVersion.js
 import React from "react";
+import styles from "./MobileVesion.module.css";
 
 const MobileVersion = () => {
   return (
-    <div>
-      <h1>This is mobile version</h1>
+    <div className={styles.container}>
+      <h1 className={styles.header}>This is mobile version</h1>
     </div>
   );
 };
